26d826dad8e6fa64ba81699b3d2376a09793104b
YordanSBarrera <yordansaldanabarrera@gmail.com>
2022-10-28 13:54:14 -0300

    add banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import  Banner from './components/Banner';
 import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     
+     <Banner/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+//import Container from "react-bootstrap/Container";
+import {Nav,Navbar,Container} from "react-bootstrap";
+//import Navbar from "react-bootstrap/Navbar";
 //import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from "../assets/img/logo.svg";
 import Icon1 from "../assets/img/nav-icon1.svg";
