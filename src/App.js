0d04ee5de7df20fa999d42c38626641b0b92707a
YordanSBarrera <yordansaldanabarrera@gmail.com>
2022-10-30 00:01:46 -0300

    skills component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects ';
-import Skills from './components/Skills';
+import {Skills} from './components/Skills';
 function App() {
   return (
     <div className="App">
