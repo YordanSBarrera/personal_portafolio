88e80b353e7cf29e3bcfdfb5d9b62eebde97c385
Yordan <68411867+YordanSBarrera@users.noreply.github.com>
2024-09-01 14:37:42 -0300

    organizing app

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,18 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo-ysb.png";
-import IconLink from "../assets/img/nav-icon1.svg";
-import IconFace from "../assets/img/nav-icon2.svg";
-import IconInst from "../assets/img/nav-icon3.svg";
+import logo from "../../assets/img/logo-ysb.png";
+import IconLink from "../../assets/img/nav-icon1.svg";
+import IconFace from "../../assets/img/nav-icon2.svg";
+import IconInst from "../../assets/img/nav-icon3.svg";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer text-center p-3 mt-auto">
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <a href="#home">
+              <img src={logo} alt="Logo" />
+            </a>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
